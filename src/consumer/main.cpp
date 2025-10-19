9104bcd630e8f789e6e29b6097e4cca8b8d48317
Monkeber <27787571+monkeber@users.noreply.github.com>
2025-10-19 20:01:29 +0300

    Add primitive structure for sending over shared memory

--- src/consumer/main.cpp
+++ src/consumer/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+
+#include <chrono>
+#include <thread>
+
+constexpr auto SHARED_MEMORY_NAME{ "/mpdataaggregator" };
+constexpr auto SHARED_MEMORY_LENGTH{ 2560 };
+
+constexpr auto DATA_CHUNK_SIZE{ 256 };
+
+struct ShBuf
+{
+	pid_t pid;
+	char data[DATA_CHUNK_SIZE];
+
+	void SetData(const char* str)
+	{
+		std::memcpy(data, str, std::strlen(str) + 1);
+	}
+};
 
 int main()
 {
-	std::cout << "Consumer" << std::endl;
+	std::cout << "Consumer Started" << std::endl;
+
+	const auto fd{ shm_open(SHARED_MEMORY_NAME, O_RDONLY | O_CREAT, 0666) };
+
+	ftruncate(fd, SHARED_MEMORY_LENGTH);
+
+	void* ptr = mmap(NULL, SHARED_MEMORY_LENGTH, PROT_READ, MAP_SHARED, fd, 0);
+
+	close(fd);
+
+	std::this_thread::sleep_for(std::chrono::seconds{ 7 });
+
+	ShBuf buf;
+	std::memcpy(&buf, ptr, sizeof(ShBuf));
+
+	std::cout << buf.pid << "\n" << buf.data << std::endl;
+
+	munmap(ptr, SHARED_MEMORY_LENGTH);
+	shm_unlink(SHARED_MEMORY_NAME);
 
 	return 0;
 }
--- src/producer/main.cpp
+++ src/producer/main.cpp
@@ -1,8 +1,53 @@
+#include <fcntl.h>
 #include <iostream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <cerrno>
+
+constexpr auto SHARED_MEMORY_NAME{ "/mpdataaggregator" };
+constexpr auto SHARED_MEMORY_LENGTH{ 2560 };
+constexpr auto DATA_CHUNK_SIZE{ 256 };
+
+struct ShBuf
+{
+	pid_t pid;
+	char data[DATA_CHUNK_SIZE];
+
+	void SetData(const char* str)
+	{
+		std::memcpy(data, str, std::strlen(str) + 1);
+	}
+};
 
 int main()
 {
-	std::cout << "Producer" << std::endl;
+	std::cout << "Producer Started" << std::endl;
+
+	const auto fd{ shm_open(SHARED_MEMORY_NAME, O_RDWR | O_CREAT, 0666) };
+
+	ftruncate(fd, SHARED_MEMORY_LENGTH);
+
+	void* ptr = mmap(NULL, SHARED_MEMORY_LENGTH, PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+
+	if (ptr == MAP_FAILED)
+	{
+		std::cout << "Mmap returned null, errno: " << errno << std::endl;
+		return 1;
+	}
+
+	ShBuf buf;
+	buf.pid = getpid();
+	buf.SetData("Hello");
+
+	std::memcpy(ptr, &buf, sizeof(ShBuf));
+
+	munmap(ptr, SHARED_MEMORY_LENGTH);
+	shm_unlink(SHARED_MEMORY_NAME);
 
 	return 0;
 }
